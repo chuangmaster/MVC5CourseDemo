ef6633bc6dff4cab1061cd40f0c9c58c2464f417
chuangmaster <chuang.master@gmail.com>
2017-12-02 13:53:01 +0800

    model驗證；Product Stock 顯示

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,7 +11,8 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,13 +20,19 @@ namespace MVC5Course.Models
         {
             this.OrderLine = new HashSet<OrderLine>();
         }
-    
+
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "商品名稱不得為空")]
         public string ProductName { get; set; }
+        [Required]
+        [Range(1, 99999, ErrorMessage = "價格必須介於1~99999")]
         public Nullable<decimal> Price { get; set; }
+        [Required]
         public Nullable<bool> Active { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString ="{0:#}")]
         public Nullable<decimal> Stock { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
