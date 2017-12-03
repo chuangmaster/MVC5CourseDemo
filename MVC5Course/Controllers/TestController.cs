8f26b54017e681b10c07c606f9e2955cf8427744
chuangmaster <chuang.master@gmail.com>
2017-12-03 10:05:56 +0800

    嘗試產生一個Product的CRUD

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,83 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+        // GET: Test
+        public ActionResult Index()
+        {
+            var result = from i in db.Product
+                         where i.IsDeleted == false
+                         orderby i.ProductId descending
+                         select i;
+            return View(result.Take(100));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = db.Product.Find(id);
+                return View(data);
+            }
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = db.Product.Find(product.ProductId);
+                data.Active = product.Active;
+                data.OrderLine = product.OrderLine;
+                data.Price = product.Price;
+                data.ProductName = product.ProductName;
+                data.Stock = product.Stock;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(db.Product.Find(id));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var productData = db.Product.Find(id);
+            //db.OrderLine.RemoveRange(productData.OrderLine.ToList());
+            //db.Product.Remove(productData);
+            productData.IsDeleted = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
