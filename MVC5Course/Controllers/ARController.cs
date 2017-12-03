b987fddfedf2e343425ec4768a9f7cca393c15b7
chuangmaster <chuang.master@gmail.com>
2017-12-03 15:57:26 +0800

    FileTest()測試

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -22,14 +22,33 @@ namespace MVC5Course.Controllers
         public ActionResult ContentTest()
         {
             //此用方避免，會降低維護
-            return Content("<script>alert('新增成功!');location.href='/';</script>");            
+            return Content("<script>alert('新增成功!');location.href='/';</script>");
         }
 
         public ActionResult ContentTestBetter()
         {
-            return PartialView("Alert","新增成功");
+            return PartialView("Alert", "新增成功");
+        }
+        public ActionResult FileTest(string dl)
+        {
+            //ContentType:請查閱 http://www.freeformatter.com/mime-types-list.html
+            if (string.IsNullOrEmpty(dl))
+            {
+                return File(Server.MapPath("~/App_Data/switch.jpg"), "image/jpeg");
+            }
+            else
+            {
+                return File(Server.MapPath("~/App_Data/switch.jpg"), "image/jpeg", "Switch.jpg");
+
+            }
         }
 
-       
+        //public ActionResult FileTest()
+        //{
+
+        //    //ContentType:請查閱 http://www.freeformatter.com/mime-types-list.html
+        //    return File(Server.MapPath("~/App_Data/switch.jpg"), "image/jpeg", "Switch.jpg");
+        //}
+
     }
 }
