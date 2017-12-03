a4b107a32dc0308dfaa5c6ecdf66d7fcd65a4aa7
chuangmaster <chuang.master@gmail.com>
2017-12-03 11:26:26 +0800

    輸入驗證與模型驗證

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -1,27 +1,40 @@
 namespace MVC5Course.Models
 {
+    using MVC5Course.Models.ValidateAttribute;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
-    public partial class Product
+    public partial class Product : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Stock >= 100 && this.Price < 1000)
+            {
+                yield return new ValidationResult("當價格低於1000，不能有超過100的庫存!", new string[] { "Stock", "Price" });
+            }
+        }
     }
-    
-    public partial class ProductMetaData
+
+    public partial class ProductMetaData 
     {
+        
         [Required]
         public int ProductId { get; set; }
-        
-        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+
+        [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
+
+        [商品名稱驗證(ErrorMessage = "商品名稱必要含[商品]")]
         public string ProductName { get; set; }
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
         public Nullable<decimal> Stock { get; set; }
         [Required]
         public bool IsDeleted { get; set; }
-    
+
         public virtual ICollection<OrderLine> OrderLine { get; set; }
+
+       
     }
 }
---
+++ MVC5Course/Models/ValidateAttribute/商品名稱驗證Attribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MVC5Course.Models.ValidateAttribute
+{
+    public class 商品名稱驗證Attribute : DataTypeAttribute
+    {
+        //private static Regex _regex = new Regex("[]", RegexOptions.IgnoreCase);
+        public 商品名稱驗證Attribute() : base(DataType.Text)
+        {
+
+        }
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            string str = (string)value;
+            //return valueAsString != null && _regex.Match(valueAsString).Length > 0;
+            return str.Contains("[商品]") ? true : false;
+        }
+    }
+}
