00c2cb7c7588807b4ddbb3bc205c7843185fa55c
chuangmaster <chuang.master@gmail.com>
2017-12-03 15:46:08 +0800

    測試View()、PartialView()、Content()

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            return PartialView("Index");
+            //return PartialView("../Home/About");
+        }
+
+        public ActionResult ContentTest()
+        {
+            //此用方避免，會降低維護
+            return Content("<script>alert('新增成功!');location.href='/';</script>");            
+        }
+
+        public ActionResult ContentTestBetter()
+        {
+            return PartialView("Alert","新增成功");
+        }
+
+       
+    }
+}
