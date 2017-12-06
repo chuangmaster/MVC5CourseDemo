4ebb7fd1991d589a1778cb34febe3e9840ba2af5
chuangmaster <chuang.master@gmail.com>
2017-12-06 23:14:49 +0800

    使用ViewData傳入資料到View

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        // GET: MV
+        public ActionResult Index()
+        {
+            //強型別傳值
+            var data = repo.GetTop10();
+            ViewData.Model = data;  //等同於View(data);
+            return View();
+        }
+    }
+}
