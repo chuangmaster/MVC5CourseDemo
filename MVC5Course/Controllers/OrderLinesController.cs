354330529f8434f530af7076f8cff9dc3400123d
chuangmaster <chuang.master@gmail.com>
2017-12-17 14:38:45 +0800

    @Html.Action練習

---
+++ MVC5Course/Controllers/OrderLinesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class OrderLinesController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: OrderLines
+        public ActionResult Index(int id)
+        {
+            var orderLine = db.OrderLine.Where(x => x.ProductId == id).Include(o => o.Order).Include(o => o.Product);
+            return View(orderLine.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
